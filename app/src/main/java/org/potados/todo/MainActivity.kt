a096fa7b76b52a43c7ecf9535c71ee91a8fa8687
potados99 <potados99@gmail.com>
2020-11-22 20:23:59 +0900

    Add add button

--- app/src/main/java/org/potados/todo/MainActivity.kt
+++ app/src/main/java/org/potados/todo/MainActivity.kt
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Debug
 import android.util.Log
+import android.view.Menu
 import androidx.activity.viewModels
+import androidx.core.view.iterator
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import org.potados.todo.data.TodoRepository
@@ -19,6 +21,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         initView()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+
+        return true
     }
 
     override fun onResume() {
